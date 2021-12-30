1d8c1cc5bf75cad47ae9bb1ce5e94663a5165856
Noushinnawal <96626935+Noushinnawal@users.noreply.github.com>
2021-12-30 23:15:27 +0600

    Add files via upload

---
+++ food-order/web-design-course-restaurant-master/admin/manage-order.php
@@ -0,0 +1,47 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="navbar">
+<div class="boxes">
+<h1> Manage Order </h1>
+<br/><br/><br/>
+            
+            <br/><br/><br/>
+            <table class="table-style">
+                <tr>
+                    <td> ID</td>
+                    <td> Full Name </td>
+                    <td> Username</td>
+                    <td>Actions </td>
+                </tr>
+                <tr>
+                    <td> 1</td>
+                    <td> David </td>
+                    <td> david22</td>
+                    <td>
+                        <a href="#" class="button-secondary">Update Admin </a>
+                        <a href="#" class="button-danger">Delete Admin </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td> 2</td>
+                    <td> David </td>
+                    <td> david22</td>
+                    <td>
+                    <a href="#" class="button-secondary">Update Admin </a>
+                        <a href="#" class="button-danger">Delete Admin </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td> 3</td>
+                    <td> David </td>
+                    <td> david22</td>
+                    <td>
+                    <a href="#" class="button-secondary">Update Admin </a>
+                        <a href="#" class="button-danger">Delete Admin </a>
+                    </td>
+                </tr>
+            </table>
+
+</div>
+</div>
+<?php include('partials/footer.php'); ?>
---
+++ food-order/web-design-course-restaurant-master/admin/partials/footer.php
@@ -0,0 +1,10 @@
+  <!-- footer Section Starts Here -->
+  <section class="footer">
+        <div class="boxes_taking_text_in_center">
+            <p>Copyright © 2021. Developed & Designed By <a href="#">Al Mehedi Hasan</a> & <a href="#">Nowshin Nawal</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
